b6e1698c2dcd27700e372919d63902abb35021dc
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-06-20 20:23:06 -0300

    Adicionar classe UsuarioController com endpoints para gerenciar usuários

---
+++ usuarioapi/src/main/java/com/firstcrud/usuarioapi/controller/UsuarioController.java
@@ -0,0 +1,52 @@
+package com.firstcrud.usuarioapi.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.firstcrud.usuarioapi.model.Usuario;
+import com.firstcrud.usuarioapi.services.UsuarioService;
+
+import jakarta.websocket.server.PathParam;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+    
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> obterTodos(){
+        return usuarioService.obterTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Usuario> obterPorId(@PathVariable Integer id){
+        return usuarioService.obterPorId(id);
+    }
+
+    @PostMapping
+    public List<Usuario> adicionar(@RequestBody Usuario usuario){
+        return usuarioService.adicionar(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public Usuario atualizar(@PathVariable Integer id, @RequestBody Usuario usuario){
+        return usuarioService.atualizar(id, usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public List<Usuario> deletar(@PathVariable Integer id){
+        return usuarioService.deletar(id);
+    }
+}
