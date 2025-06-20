e3e8289cedd18b5d1f41184c580aa30dccac33cb
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-06-20 18:58:52 -0300

    Adicionar estrutura inicial do projeto com configuração Maven e setup do Spring Boot

---
+++ usuarioapi/src/test/java/com/firstcrud/usuarioapi/UsuarioapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstcrud.usuarioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
